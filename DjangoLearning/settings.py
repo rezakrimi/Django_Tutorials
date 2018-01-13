1a99f46190fdc056304281ae08b7cbd37fa0f649
Reza Karimi <reza@Rezas-MacBook-Pro.local>
2018-01-13 18:32:54 -0500

    index page extends an html file for for navigation bar purposes

--- DjangoLearning/settings.py
+++ DjangoLearning/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'webapp',
+    'personal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- DjangoLearning/urls.py
+++ DjangoLearning/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^webapp/', include('webapp.urls')),
+    re_path(r'^$', include('personal.urls')),
 ]
